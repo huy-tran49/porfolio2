1b3ba7d1e28acd8de19301cfd3915d0ee93604a1
Huy <h.qt193@gmail.com>
2023-08-19 10:50:04 -0700

    added dependencies

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <section className="w-full flex-center flex-col">
+            <h1 className="head_text text-center">Huy Tran</h1>
+            <br className="max-md:hidden"/>
+        </section>
+    )
+}
+
+export default Home
